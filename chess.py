13dd8858258b985b39ff35e339556dd2e7b0f375
Mateusz Jurewicz <mateusz.jurewicz@gmail.com>
2019-04-11 17:14:13 +0200

    Add a simple board object

--- chess.py
+++ chess.py
@@ -1,8 +1,28 @@
 class Game:
     pass
 
-class Board:
-    pass
 
 class Piece:
     pass
+
+
+class Board(object):
+    """
+    A chess board object.
+    """
+
+    def __init__(self, num_columns, num_rows):
+        self.num_columns = num_columns
+        self.num_rows = num_rows
+
+    def __repr__(self):
+        return "repr"
+
+    def __str__(self):
+        return str(self.num_columns)
+
+
+if __name__ == "__main__":
+
+    our_board = Board(10, 10)
+    print(our_board)
